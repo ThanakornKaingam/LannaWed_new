fe9a38506453b56d169f32605ebfd1fda5b76311
ThanakornKaingam <thanakornkaingam@gmail.com>
2026-03-15 11:35:59 +0700

    Update model_loader.py

--- backend/app/ml/model_loader.py
+++ backend/app/ml/model_loader.py
@@ -1,5 +1,6 @@
 import torch
 from pathlib import Path
+import os
 
 _model = None
 
@@ -7,7 +8,15 @@ def load_model():
     global _model
 
     if _model is None:
+
         model_path = Path(__file__).resolve().parent / "MobileNetV3-Large.pt"
+
+        if not model_path.exists():
+            print("MODEL NOT FOUND:", model_path)
+            return None
+
+        print("Loading model:", model_path)
+
         _model = torch.jit.load(str(model_path), map_location="cpu")
         _model.eval()
 
